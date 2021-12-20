627a3b112e593612b99982217078c5a1e48a7515
josedavid2021 <jmolinerdev@gmail.com>
2021-12-20 17:30:45 +0100

    index, heade y footer.php

--- index.php
+++ index.php
@@ -1,24 +1,4 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Index</title>
-</head>
-<body>
-
-<div>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga saepe velit beatae magnam amet placeat perspiciatis, quisquam animi eos quaerat eligendi voluptatem atque commodi quas aut maiores! Ipsum, autem illum!</div>
-    
-<?php
-$nombre = "Jose D.";
-//echo "<div>mi nombre es ".$nombre." y esto es un div con <strong>strong</strong>!!</div>";
-?>
+<?php get_header(); ?>
 
 
-
-<div>Mi nombre es <?php echo $nombre; ?> y esto es un div con <strong>strong</strong></div>
-
-
-</body>
-</html>
+<?php get_footer(); ?>
