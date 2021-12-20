5e38207ac1d962b6c9e98682e9ae0c63ef67a8b8
josedavid2021 <jmolinerdev@gmail.com>
2021-12-20 17:31:12 +0100

    footer y header.php

---
+++ footer.php
@@ -0,0 +1,9 @@
+    <div class="bg-black text-white py-8">
+        <footer class="container px-10">
+            Footer - 2022 ©
+        </footer>        
+    </div>
+
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Mis recetas | misrecetas.com</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Mis recetas de cocina casera">
+    <meta name="keywords" content="recetas, cocina casera, gastronomía">
+    <link rel="shortcut icon" href="huevo-frito.png" type="image/x-icon">
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body>
+
+    hola que tal soy el header
+
+    <div class="container mx-auto px-10">
+        <header class="flex justify-center my-5">
+            <img class="h-16 sm:h-28" src="images/logo.png" alt="">
+        </header>
+    </div>
+
+    <div class="bg-gray-300 py-2 sm:py-4 mb-10">
+        <nav class="container mx-auto px-10 text-center text-lg">
+            <span class="text-gray-400">Inicio</span>
+            <span class="text-gray-500 mx-2 text-base">|</span>
+            <a class="hover:underline" href="recetas.html">Recetas</a>
+            <span class="text-gray-500 mx-2 text-base">|</span>
+            <a class="hover:underline" href="contactar.html">Contactar</a>
+        </nav>
+    </div>
